274c5a4ffdddddea3eb2ad35b77f27b7fa10ccf0
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-09-19 05:25:19 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,18 @@
 class Waiter
-
+  attr_accessor :name , :years
+  @@all = []
+  
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def best_tipper 
+    Meal.all.find_all{ |meal| meal.waiter == self}.sort_by{|meal| meal.tip}.last.customer
+  end
 end
